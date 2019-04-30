1f81e2d3b3da4ff4e03b9e349941436ae5ff5cfe
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-04-30 11:57:31 +0200

    alert for add list

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { TasksService } from '../../services/tasks.service';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -9,12 +10,39 @@ import { Router } from '@angular/router';
 })
 export class Tab1Page {
 
-  constructor(public tasksService: TasksService, private router: Router) {
+  constructor(public tasksService: TasksService, private router: Router,
+    private alertController: AlertController) {
 
 
   }
 
-  addList() {
-    this.router.navigateByUrl('/tabs/tab1/add');
+  async addList() {
+    // this.router.navigateByUrl('/tabs/tab1/add');
+    const alert = await this.alertController.create({
+      header: 'New list',
+      inputs: [{
+        name: 'title',
+        type: 'text',
+        placeholder: 'List name'
+      }],
+      buttons: [{
+        text: 'Cancel',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel');
+        }
+      },
+      {
+        text: 'Add',
+        handler: (data) => {
+          if (data.title.length === 0) {
+            return;
+          }
+
+        }
+      }]
+    });
+
+    await alert.present();
   }
 }
