b43e32aa0cf63c9a16a00ec10dcddcf85cc6fd8e
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-05-01 15:05:05 +0200

    working on add tasks to list

--- src/app/pages/add/add.page.ts
+++ src/app/pages/add/add.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { TasksService } from '../../services/tasks.service';
+import { ActivatedRoute } from '@angular/router';
+import { List } from '../../models/list.model';
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-add',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPage implements OnInit {
 
-  constructor() { }
+  list: List;
+  taskName = '';
+  constructor(private tasksService: TasksService, private router: ActivatedRoute) {
+
+    const idList = this.router.snapshot.paramMap.get('idList');
+
+    this.list = this.tasksService.getList(idList);
+  }
 
   ngOnInit() {
   }
 
+  addItem() {
+    if (this.taskName.length === 0) { return; }
+     const newTask = new Task(this.taskName);
+     this.list.tasks.push
+  }
 }
+ 
--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -22,6 +22,12 @@ export class TasksService {
     return newList.id;
   }
 
+  getList(id: string | number) {
+    id = Number(id);
+    return this.lists.find(listData => listData.id === id);
+  }
+
+
   saveStorage() {
     localStorage.setItem('data', JSON.stringify(this.lists));
 
